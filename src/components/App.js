4091923b81af2f6848e94067462dd09396481000
JohnHernCode <john@writecode.ninja>
2021-06-30 11:24:20 -0600

    Set up ButtonPanel.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       result: '',
