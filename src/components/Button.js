6c150b55c9869fad16fed24ba5c3fa433290436c
JohnHernCode <john@writecode.ninja>
2021-06-30 10:54:17 -0600

    Build the button.js

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = (props) => {
+  const {
+    name, clickHandler,
+  } = props;
+  return (
+    <button
+      type="button"
+      onClick={() => clickHandler(name)}
+    >
+      {name}
+    </button>
+  );
+};
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired,
+};
+
+Button.defaultProps = {
+};
+
+export default Button;
