27b4974078228824ff1e1809128c659f4956d5ed
JohnHernCode <john@writecode.ninja>
2021-07-08 16:33:59 -0600

    Add styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,9 @@
 // import React, { Component } from 'react';
 import React, { useState, useEffect } from 'react';
-import '../App.css';
+import styles from '../styles/App.module.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//
-//     this.state = {
-//       total: null,
-//       next: null,
-//       operation: null,
-//     };
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-//
-//   handleClick = (buttonName) => {
-//     const { total, next, operation } = calculate(this.state, buttonName);
-//     this.setState({
-//       total,
-//       next,
-//       operation,
-//     });
-//   }
-//
-//   render() {
-//     let calc;
-//     const { next, total } = this.state;
-//     if (next !== null) {
-//       calc = next;
-//     } else if (total !== null) {
-//       calc = total;
-//     }
-//     return (
-//       <div className="app">
-//         <Display calc={calc} />
-//         <ButtonPanel clickHandler={this.handleClick} />
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
 
 const App = () => {
   const [total, setTotal] = useState(null);
@@ -71,9 +31,11 @@ const App = () => {
 
   return (
     <>
-      <div className="app">
-        <Display calc={calc} />
-        <ButtonPanel clickHandler={handleClick} />
+      <div className={styles.container}>
+        <div className={styles.app}>
+          <Display calc={calc} />
+          <ButtonPanel clickHandler={handleClick} />
+        </div>
       </div>
     </>
   );
