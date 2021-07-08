3c9ac836cf56160c0be3d6c9116b9185a751a263
JohnHernCode <john@writecode.ninja>
2021-07-08 07:12:41 -0600

    Refactor to use hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,82 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       total: null,
+//       next: null,
+//       operation: null,
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+//
+//   handleClick = (buttonName) => {
+//     const { total, next, operation } = calculate(this.state, buttonName);
+//     this.setState({
+//       total,
+//       next,
+//       operation,
+//     });
+//   }
+//
+//   render() {
+//     let calc;
+//     const { next, total } = this.state;
+//     if (next !== null) {
+//       calc = next;
+//     } else if (total !== null) {
+//       calc = total;
+//     }
+//     return (
+//       <div className="app">
+//         <Display calc={calc} />
+//         <ButtonPanel clickHandler={this.handleClick} />
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
 
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const App = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+  const [calc, setCalc] = useState(null);
 
-  handleClick = (buttonName) => {
-    const { total, next, operation } = calculate(this.state, buttonName);
-    this.setState({
-      total,
-      next,
-      operation,
-    });
-  }
+  const handleClick = (buttonName) => {
+    const result = calculate({ total, next, operation }, buttonName);
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
+  };
 
-  render() {
-    let calc;
-    const { next, total } = this.state;
-    if (next !== null) {
-      calc = next;
-    } else if (total !== null) {
-      calc = total;
+  useEffect(() => {
+    if (operation === null) {
+      setCalc(<Display calc="0" />);
     }
-    return (
+    if (next !== null) {
+      setCalc(next);
+    } else if (total !== null) {
+      setCalc(total);
+    }
+  }, [total, next]);
+
+  return (
+    <>
       <div className="app">
         <Display calc={calc} />
-        <ButtonPanel clickHandler={this.handleClick} />
+        <ButtonPanel clickHandler={handleClick} />
       </div>
-    );
-  }
-}
+    </>
+  );
+};
 
 export default App;
