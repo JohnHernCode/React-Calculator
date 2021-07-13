d2cc5d47a25864670feca7ae78ff8f8337ad2805
JohnHernCode <john@writecode.ninja>
2021-07-13 08:25:45 -0600

    Add home page

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="home">
+    <h2 className="home-header">Welcome to our page!</h2>
+    <p className="home-text">
+      Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit, sed do eiusmod tempor
+      incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation
+      ullamco laboris nisi ut aliquip ex ea commodo consequat
+      Duis aute irure dolor in reprehenderit in voluptate velit
+      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui o!cia deserunt
+      mollit anim id est laborum. IpsuLorem ipsum dolor sit amet, consectetur
+      adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui o!cia deserunt mollit anim id est laborum. Ipsu
+    </p>
+  </div>
+);
+
+export default Home;
