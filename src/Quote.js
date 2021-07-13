9b461ad12e91c983927ef62c4749797760876545
JohnHernCode <john@writecode.ninja>
2021-07-13 08:33:09 -0600

    Add Qoute page

---
+++ src/Quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="quote">
+    <h1>Quote of the day</h1>
+    <p className="quote-text">
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding. –William Paul ThurstonHomeCalculatorQuote
+
+    </p>
+  </div>
+);
+
+export default Quote;
